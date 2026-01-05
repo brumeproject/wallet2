5595e414b0ad957192a1a731a68d34bbed219313
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-01-05 19:12:29 +0100

    f

--- src/libs/errors/mod.ts
+++ src/libs/errors/mod.ts
@@ -5,10 +5,13 @@ export namespace Errors {
   export function display(error: unknown) {
     console.error(error)
 
-    if (error instanceof Error)
-      return alert(`An error occured: ${error.message}`)
+    if (error instanceof Error === false)
+      return alert("An unknown error occured")
 
-    alert("An unknown error occured")
+    if (!error.message.length)
+      return alert("An unknown error occured")
+
+    alert(`An error occured: ${error.message}`)
   }
 
 }
