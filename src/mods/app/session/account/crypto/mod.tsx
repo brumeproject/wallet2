c7d7a8bd02def598cbcc1df2e33d08346acfa5d3
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-03-19 23:25:12 +0700

    c

--- src/mods/app/session/account/crypto/mod.tsx
+++ src/mods/app/session/account/crypto/mod.tsx
@@ -143,7 +143,7 @@ export function CryptoAccountPage(props: { $entry: KDBX.Inner.KeePassFile.Entry 
           Ethereum address
         </div>
         <div className="text-default-contrast">
-          Your Ethereum address (Ethereum, Base, Polygon, etc.)
+          Your Ethereum address (EVM, ERC20, Base, Polygon, etc.)
         </div>
         <div className="h-4" />
         <div className="bg-default-contrast po-2 rounded-xl flex flex-col gap-4 [&:has(:focus-visible)]:outline-2 [&:has(:focus-visible)]:outline-offset-2 [&:has(:focus-visible)]:outline-default-contrast">
@@ -160,7 +160,7 @@ export function CryptoAccountPage(props: { $entry: KDBX.Inner.KeePassFile.Entry 
           Solana address
         </div>
         <div className="text-default-contrast">
-          Your Solana address (Solana, Eclipse, Atlas, etc.)
+          Your Solana address (SVM, Eclipse, Atlas, etc.)
         </div>
         <div className="h-4" />
         <div className="bg-default-contrast po-2 rounded-xl flex flex-col gap-4 [&:has(:focus-visible)]:outline-2 [&:has(:focus-visible)]:outline-offset-2 [&:has(:focus-visible)]:outline-default-contrast">
