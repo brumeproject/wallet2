b37d649d1a6d4218639f6d28721e71c804505fac
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-03-24 12:04:25 +0700

    f

--- src/mods/app/session/account/crypto/mod.tsx
+++ src/mods/app/session/account/crypto/mod.tsx
@@ -204,7 +204,7 @@ export function CryptoAccountPage(props: { $entry: KDBX.Inner.KeePassFile.Entry 
                   </div>
                 </div>
               </a>
-              <a className="group w-[320px] aspect-video p-4 z-10 rounded-xl bg-default text-default border-2 border-default-contrast select-none -translate-y-60 hover:translate-x-3 focus-visible:outline-none focus-visible:translate-x-3 transition-transform"
+              <a className="group w-[320px] aspect-video z-10 rounded-xl bg-default text-default border-2 border-default-contrast select-none -translate-y-60 hover:translate-x-3 focus-visible:outline-none focus-visible:translate-x-3 transition-transform"
                 href={hash.go("/add").hash}>
                 <InAnchor>
                   <Outline.PlusIcon className="size-8" />
