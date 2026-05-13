f88552dfc2cb173604a247314023a4da56e4df6a
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-13 12:03:23 +0700

    f

--- src/mods/app/session/account/crypto/request/mod.tsx
+++ src/mods/app/session/account/crypto/request/mod.tsx
@@ -44,7 +44,7 @@ export function CryptoRequestAnchor(props: { $entry: KDBX.Inner.KeePassFile.Entr
           <CryptoRequestPage $entry={$entry} subaccount={subaccount} title={title} session={session} request={request} />
         </PathBoard>}
     </SubpathProvider>
-    <a className="group w-[min(320px,100%)] aspect-video p-4 z-10 rounded-xl bg-default text-default border-2 border-default-contrast select-none flex flex-col hover:translate-x-3 focus-visible:outline-none focus-visible:translate-x-3 transition-transform
+    <a className="relative group w-[min(320px,100%)] aspect-video p-4 z-10 rounded-xl bg-default text-default border-2 border-default-contrast select-none flex flex-col hover:translate-x-3 focus-visible:outline-none focus-visible:translate-x-3 transition-transform
       data-[color=red]:bg-red-400 
       data-[color=orange]:bg-orange-400 
       data-[color=amber]:bg-amber-400 
@@ -85,16 +85,16 @@ export function CryptoRequestAnchor(props: { $entry: KDBX.Inner.KeePassFile.Entr
       href={coords.url.hash}
       onClick={coords.onClick}
       onKeyDown={coords.onKeyDown}>
+      <div className="absolute top-0 right-0 -translate-y-1.5 translate-x-1.5 flex size-4">
+        <div className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75" />
+        <div className="relative inline-flex size-4 rounded-full bg-sky-500" />
+      </div>
       <div className="flex items-center justify-between">
         <div className="font-medium text-xl truncate">
           {title || Lang.match({ en: "Untitled", zh: "无标题", hi: "शीर्षक रहित", es: "Sin título", ar: "بدون عنوان", fr: "Sans titre", de: "Unbenannt", ru: "Без названия", pt: "Sem título", ja: "無題", pa: "ਬਿਨਾਂ ਸਿਰਲੇਖ ਦੇ", bn: "বিনা শিরোনাম", id: "Tanpa judul", ur: "بغیر عنوان کے", ms: "Tanpa judul", it: "Senza titolo", tr: "Başlıksız", ta: "தலைப்பு இல்லாமல்", te: "శీర్షిక లేని", ko: "제목 없음", vi: "Không tiêu đề", pl: "Bez tytułu", ro: "Fără titlu", nl: "Ongetiteld", el: "Χωρίς τίτλο", th: "ไม่มีชื่อเรื่อง", cs: "Nezvaný", hu: "Névtelen", sv: "Otitulerad", da: "Uden titel" })}
         </div>
         <div className="font-medium text-xl text-default-half-contrast">
           #{subaccount + 1}
-        </div>
-        <div className="absolute top-0 right-0 -translate-y-1.5 translate-x-1.5 flex size-4">
-          <div className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75" />
-          <div className="relative inline-flex size-4 rounded-full bg-sky-500" />
         </div>
       </div>
     </a>
