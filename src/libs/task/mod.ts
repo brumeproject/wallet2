e9663b0671e4c77028362ab436d9e2f283bcfbd8
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-05-21 12:43:19 +0700

    c

--- src/libs/task/mod.ts
+++ src/libs/task/mod.ts
@@ -1,6 +1,12 @@
 import { Awaitable } from "@/libs/awaitable/mod.ts";
 import { DependencyList, useCallback, useMemo, useState } from "react";
 
+/**
+ * A hook to execute an async function once and track its execution state
+ * @param callback 
+ * @param deps 
+ * @returns 
+ */
 export function useSubmit<P extends readonly unknown[]>(callback: (...params: P) => Awaitable<void>, deps: DependencyList) {
   const [running, setRunning] = useState(false)
 
