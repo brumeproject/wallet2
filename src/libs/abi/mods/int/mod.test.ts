23f4bd0980095d90d797dcd6ae3143e94b338751
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-06-03 23:27:08 +0700

    t

--- src/libs/abi/mods/int/mod.test.ts
+++ src/libs/abi/mods/int/mod.test.ts
@@ -1,18 +1,19 @@
-import { AbiInt32 } from "@/libs/abi/mods/int/mod.ts";
+import { AbiInt8 } from "@/libs/abi/mods/int/mod.ts";
 import { Readable, Writable } from "@hazae41/binary";
 import { test } from "@hazae41/phobos";
 
 function f(value: bigint) {
-  const raw = Writable.writeToBytes(AbiInt32.from(value))
+  const raw = Writable.writeToBytes(AbiInt8.from(value))
 
   console.log(raw.toHex())
 
-  console.log(value, Readable.readFromBytes(AbiInt32, raw).into())
+  console.log(value, Readable.readFromBytes(AbiInt8, raw).into())
 }
 
 test("int", () => {
   f(42n)
-  f(-100n)
+  f(127n)
+  f(-127n)
   f(-1n)
-  f(1n)
+  f(129n)
 })
