b32e1f80751533caf0d7732e2dfce97d3dfb381f
hazae41 <4405263+hazae41@users.noreply.github.com>
2026-06-19 10:48:16 +0700

    c

--- src/libs/ed25519/mod.ts
+++ src/libs/ed25519/mod.ts
@@ -1,19 +1,21 @@
+import { Cursor } from "@hazae41/cursor";
+
 export namespace ed25519 {
 
   export async function publish(keyraw: Uint8Array<ArrayBuffer>) {
     const asn = new Uint8Array([48, 46, 2, 1, 0, 48, 5, 6, 3, 43, 101, 112, 4, 34, 4, 32])
 
-    const keyasn = new Uint8Array(asn.length + keyraw.length)
-    keyasn.set(asn, 0)
-    keyasn.set(keyraw, asn.length)
+    const $keyasn = new Cursor(new Uint8Array(asn.length + keyraw.length))
+    $keyasn.write(asn)
+    $keyasn.write(keyraw)
 
-    const keyref = await crypto.subtle.importKey("pkcs8", keyasn, { name: "Ed25519" }, true, ["sign"])
+    const keyref = await crypto.subtle.importKey("pkcs8", $keyasn.bytes, "Ed25519", true, ["sign"])
     const keyjwk = await crypto.subtle.exportKey("jwk", keyref)
 
     delete keyjwk.d
     delete keyjwk.key_ops
 
-    const pubref = await crypto.subtle.importKey("jwk", keyjwk, { name: "Ed25519" }, true, ["verify"])
+    const pubref = await crypto.subtle.importKey("jwk", keyjwk, "Ed25519", true, ["verify"])
     const pubraw = new Uint8Array(await crypto.subtle.exportKey("raw", pubref))
 
     return pubraw
@@ -22,11 +24,11 @@ export namespace ed25519 {
   export async function sign(keyraw: Uint8Array<ArrayBuffer>, msgraw: Uint8Array<ArrayBuffer>) {
     const asn = new Uint8Array([48, 46, 2, 1, 0, 48, 5, 6, 3, 43, 101, 112, 4, 34, 4, 32])
 
-    const keyasn = new Uint8Array(asn.length + keyraw.length)
-    keyasn.set(asn, 0)
-    keyasn.set(keyraw, asn.length)
+    const $keyasn = new Cursor(new Uint8Array(asn.length + keyraw.length))
+    $keyasn.write(asn)
+    $keyasn.write(keyraw)
 
-    const keyref = await crypto.subtle.importKey("pkcs8", keyasn, { name: "Ed25519" }, true, ["sign"])
+    const keyref = await crypto.subtle.importKey("pkcs8", $keyasn.bytes, "Ed25519", true, ["sign"])
     const sigraw = new Uint8Array(await crypto.subtle.sign("Ed25519", keyref, msgraw))
 
     return sigraw
